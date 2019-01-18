2a21b134d0d20fb9e7a87b567b92621141cc7d9d
MarioRuiz <MarioRuizS@gmail.com>
2019-01-18 13:12:15 +0000

    modifed ports and hosts in tests to run faster

--- spec/nice_http_spec.rb
+++ spec/nice_http_spec.rb
@@ -7,18 +7,18 @@ RSpec.describe NiceHttp do
   describe 'port' do
     it 'uses the class port by default' do
       klass.host = 'localhost'
-      klass.port = 123
-      expect(klass.new.port).to eq 123
+      klass.port = 8888
+      expect(klass.new.port).to eq 8888
     end
     it 'uses the URI default when provided a URI and the URI has one' do
-      klass.port = 123
+      klass.port = 8888
       expect(klass.new('https://example.com').port).to eq 443
-      expect(klass.new('lol://example.com').port).to eq 123
+      expect(klass.new('lol://localhost').port).to eq 8888
     end
     it 'can be provided an explicit port' do
-      klass.port = 123
+      klass.port = 8888
       klass.host = 'localhost'
-      expect(klass.new(port: 456).port).to eq 456
+      expect(klass.new(port: 443).port).to eq 443
     end
     it 'raises an error when it can\'t figure out the port' do
       klass.port = nil
@@ -33,10 +33,10 @@ RSpec.describe NiceHttp do
       expect(klass.port).to eq 80
     end
     specify 'I can set/get them with accessors' do
-      expect { klass.port = 123 }.to change { klass.port }.to(123)
+      expect { klass.port = 8888 }.to change { klass.port }.to(8888)
     end
     specify 'I can set many at once with a hash' do
-      expect { klass.defaults = { port: 123 } }.to change { klass.port }.to(123)
+      expect { klass.defaults = { port: 8888 } }.to change { klass.port }.to(8888)
     end
     specify 'setting many at once doesn\'t override unprovided values' do
       expect { klass.defaults = { host: 'http://whatevz.com' } }
