5236cd6a9da9ebc4f58eba8a284d0f958a2cc401
Mario Ruiz <MarioRuizS@gmail.com>
2019-01-20 20:21:36 +0000

    added tests for put method

--- spec/nice_http_put_spec.rb
+++ spec/nice_http_put_spec.rb
@@ -1,26 +1,27 @@
 require 'nice_http'
 
-RSpec.describe NiceHttp, '#post' do
+RSpec.describe NiceHttp, '#put' do
 
     before do
         @http = NiceHttp.new('https://www.reqres.in')
     end
     
     it 'accepts hash including keys :data and :path' do
-        resp = @http.post( {
-            path: "/api/users",
-            data: { name: "morpheus", job: "leader" } 
+        resp = @http.put( {
+            path: "/api/users/2",
+            data: { name: "morpheus", job: "HR leader" } 
         } )
-        expect(resp.code).to eq 201
+        expect(resp.code).to eq 200
+        expect(resp.data.json(:job)).to eq "HR leader"
     end
 
     it 'accepts parameters as array of path, data, headers' do
-        resp = @http.post( "/api/users", "{ 'name': 'morpheus', 'job': 'leader' }", {} )
-        expect(resp.code).to eq 201
+        resp = @http.put( "/api/users/2", "{ 'name': 'morpheus', 'job': 'leader2' }", {} )
+        expect(resp.code).to eq 200
     end
 
     it 'returns error in case no path' do
-        resp = @http.post({data: { name: "morpheus", job: "leader" }})
+        resp = @http.put({data: { name: "morpheus", job: "leader" }})
         expect(resp.class).to eq Hash
         expect(resp.fatal_error).to match /no[\w\s]+path/i
         expect(resp.code).to eq nil
@@ -29,18 +30,18 @@ RSpec.describe NiceHttp, '#post' do
     end
 
     it 'accepts data_examples array in case no data supplied' do
-        resp = @http.post( {
-            path: "/api/users",
+        resp = @http.put( {
+            path: "/api/users/2",
             data_examples: [{ name: "doopy", job: "loope" }] 
         } )
-        expect(resp.code).to eq 201
+        expect(resp.code).to eq 200
         expect(resp.data.json(:name)).to eq 'doopy'
     end
     
     it 'returns the mock response if specified' do
         @http.use_mocks = true
         request = {
-            path: "/api/users",
+            path: "/api/users/2",
             data: { name: "morpheus", job: "leader" },
             mock_response: {
                 code: 100,
@@ -48,7 +49,7 @@ RSpec.describe NiceHttp, '#post' do
             }
         } 
         
-        resp = @http.post(request)
+        resp = @http.put(request)
         expect(resp.class).to eq Hash
         expect(resp.code).to eq 100
         expect(resp.message).to eq 'mock'    
@@ -61,8 +62,8 @@ RSpec.describe NiceHttp, '#post' do
         }
 
         request.values_for = {name: "peter"}
-        resp = @http.post(request)
-        expect(resp.code).to eq 201
+        resp = @http.put(request)
+        expect(resp.code).to eq 200
         expect(resp.data.json(:name)).to eq 'peter'
     end
 
