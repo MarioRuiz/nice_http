c93b30f4f39080f6d1b6990d68ad9837f90a0707
MarioRuiz <MarioRuizS@gmail.com>
2023-01-29 17:28:47 +0000

    arrays for set lambdas

--- lib/nice_http/utils/set_lambdas.rb
+++ lib/nice_http/utils/set_lambdas.rb
@@ -8,30 +8,33 @@ module NiceHttpUtils
   def self.set_lambdas(data, data_orig)
     data = data.dup
     data_orig = data_orig.dup unless data_orig.nil?
-    
-    data.each do |k, v|
-      if v.is_a?(Proc)
-        data_kv = v.call
-        if data_kv.nil?
-          data.delete(k) unless data_orig.is_a?(Hash) and data_orig.key?(k)
+    if data.is_a?(Hash)
+      data.each do |k, v|
+        if v.is_a?(Proc)
+          data_kv = v.call
+          if data_kv.nil?
+            data.delete(k) unless data_orig.is_a?(Hash) and data_orig.key?(k)
+          else
+            data[k] = data_kv
+          end
+        elsif v.is_a?(Hash)
+          data[k] = set_lambdas(v, data_orig[k])
+        elsif v.is_a?(Array)
+          v.each.with_index do |v2, i|
+              if data_orig.key?(k) and data_orig[k].is_a?(Array) and data_orig[k].size > i
+                  data[k][i] = set_lambdas(v2, data_orig[k][i])
+              else
+                  data[k][i] = set_lambdas(v2, nil)
+              end
+          end
         else
-          data[k] = data_kv
+          data[k] = v
         end
-      elsif v.is_a?(Hash)
-        data[k] = set_lambdas(v, data_orig[k])
-      elsif v.is_a?(Array)
-        v.each.with_index do |v2, i|
-            if data_orig.key?(k) and data_orig[k].is_a?(Array) and data_orig[k].size > i
-                data[k][i] = set_lambdas(v2, data_orig[k][i])
-            else
-                data[k][i] = set_lambdas(v2, nil)
-            end
-        end
-      else
-        data[k] = v
       end
+      data = data_orig.nice_merge(data) unless data_orig.nil?
+    elsif data.is_a?(Proc)
+      data = data.call  
     end
-    data = data_orig.nice_merge(data) unless data_orig.nil?
     return data
   end
 end
